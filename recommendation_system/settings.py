da7db4297a645de07d6cdceb98fa347009c3fae5
RajeRahul <rajerahul.098@gmail.com>
2020-06-19 16:38:55 +0530

    2nd review done

--- recommendation_system/settings.py
+++ recommendation_system/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 #adding additional folder name to the static  url
-STATIC_URL = '/static/recommendation_webapp/'
+STATIC_URL = '/static/'
